903c306e5b5ec4843b7da594f82915e036b9fa2a
LaxmanKumar22 <S545015@NWMISSOURI.EDU>
2021-11-11 00:42:58 -0600

    edited

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 var kiteRouter = require('./routes/kite');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
-
+var kite = require("./models/kite");
+var resourceRouter = require('./routes/resource');
 var app = express();
 
 // view engine setup
@@ -21,11 +23,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const connectionString =  process.env.MONGO_CON 
+mongoose = require('mongoose'); 
+mongoose.connect(connectionString,  
+{useNewUrlParser: true, 
+useUnifiedTopology: true}); 
+
 app.use('/', indexRouter);
 app.use('/kite', kiteRouter);
 app.use('/users', usersRouter);
 app.use('/selector', selectorRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,3 +53,44 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+//Get the default connection 
+var db = mongoose.connection; 
+ 
+//Bind connection to error event  
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+ console.log("Connection to DB succeeded")}); 
+
+ // We can seed the collection if needed on server start 
+async function recreateDB(){ 
+  // Delete everything 
+  await kite.deleteMany(); 
+ 
+  let instance1 = new 
+kite({brand:"d",  color:'pink', 
+price:25.4}); 
+  instance1.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("First object saved") 
+  }); 
+
+  let instance2 = new 
+kite({brand:"e",  color:'purple', 
+price:25.4}); 
+  instance2.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("Second object saved") 
+  }); 
+
+  let instance3 = new 
+kite({brand:"r",  color:'orange', 
+price:25.4}); 
+  instance3.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("Third object saved") 
+  }); 
+} 
+ 
+let reseed = true; 
+if (reseed) { recreateDB();} 
