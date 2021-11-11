7a2fde934c2640b440da2cb37b9c44dd54063fbe
LaxmanKumar22 <S545015@NWMISSOURI.EDU>
2021-11-11 00:43:30 -0600

    edited

--- routes/kite.js
+++ routes/kite.js
@@ -1,5 +1,13 @@
 var express = require('express');
+
+
+
+const kite_controlers= require('../controllers/kite'); 
 var router = express.Router();
+
+/* GET kites */
+ router.get('/', kite_controlers.kite_view_all_Page );
+module.exports = router;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
