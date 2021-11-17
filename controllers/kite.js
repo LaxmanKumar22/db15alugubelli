4faec057d7f8b7379a5b41b191ba84cd4e9dacb5
LaxmanKumar22 <S545015@NWMISSOURI.EDU>
2021-11-17 11:30:47 -0600

    updated

--- controllers/kite.js
+++ controllers/kite.js
@@ -1,61 +1,93 @@
 var kite = require('../models/kite');
 
 // for a specific kite.
-exports.kite_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: kite detail: ' + req.params.id);
+exports.kite_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await kite.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 
 // VIEWS 
 // Handle a show all view 
-exports.kite_view_all_Page = async function(req, res) { 
-    try{ 
-        thekites = await kite.find(); 
-        res.render('kite', { title: 'kite Search Results', results: thekites }); 
-    } 
-    catch(err){ 
-        res.status(500); 
-        res.send(`{"error": ${err}}`); 
-    }   
-}; 
+exports.kite_view_all_Page = async function (req, res) {
+    try {
+        thekites = await kite.find();
+        res.render('kite', { title: 'kite Search Results', results: thekites });
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
 
 // Handle kite delete form on DELETE.
-exports.kite_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: kite delete DELETE ' + req.params.id);
+exports.kite_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await kite.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
 };
 // Handle kite update form on PUT.
-exports.kite_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: kite update PUT' + req.params.id);
+//Handle kite update form on PUT.
+exports.kite_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await kite.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.Brand)
+            toUpdate.Brand = req.body.Brand;
+        if (req.body.color) toUpdate.color = req.body.color;
+        if (req.body.price) toUpdate.price = req.body.price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+    }
 };
 
 // List of all kites 
-exports.kite_list = async function(req, res) { 
-    try{ 
-        thekites = await kite.find(); 
-        res.send(thekites); 
-    } 
-    catch(err){ 
-        res.status(500); 
-        res.send(`{"error": ${err}}`); 
-    }   
-}; 
+exports.kite_list = async function (req, res) {
+    try {
+        thekites = await kite.find();
+        res.send(thekites);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
 
 // Handle kite create on POST.
- exports.kite_create_post = async function(req, res) {
+exports.kite_create_post = async function (req, res) {
     console.log(req.body)
     let document = new kite();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
     // {"kite_type":"goat", "cost":12, "size":"large"}
-     document.brand = re.body.brand;
-     document.color = req.body.color;
-      document.price = req.body.price;
-    try{
-    let result = await document.save();
-    res.send(result);
+    document.brand = re.body.brand;
+    document.color = req.body.color;
+    document.price = req.body.price;
+    try {
+        let result = await document.save();
+        res.send(result);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
+};
